7a52ecb02fb003fbfbcd6d2f625dd8f7d5850634
tirednemo <ummetasnim@iut-dhaka.edu>
2023-04-10 19:27:33 +0600

    remember me

--- app/src/main/java/com/example/buildbudget/AuthenticationActivity.java
+++ app/src/main/java/com/example/buildbudget/AuthenticationActivity.java
@@ -1,6 +1,5 @@
 package com.example.buildbudget;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.TextView;
--- app/src/main/java/com/example/buildbudget/MainActivity.java
+++ app/src/main/java/com/example/buildbudget/MainActivity.java
@@ -6,15 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    AuthenticationPagerAdapter adapter;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
         new Handler().postDelayed(() -> {
-            Intent start = new Intent(getApplicationContext(), TutorialActivity.class);
+            Intent start;
+            if (currentUser != null) {
+                start = new Intent(this, DashboardActivity.class);
+                start.putExtra("com.example.buildbudget.user", currentUser);
+            } else {
+                start = new Intent(getApplicationContext(), TutorialActivity.class);
+            }
             startActivity(start);
             finish();
         }, 2000);
